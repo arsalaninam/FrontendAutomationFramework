02f934e511974efb2338e0a04f4b8819cb47b075
Arsalan Inam <m.arsalaninam@gmail.com>
2019-11-07 01:20:28 +0500

    Add common methods in Test Base

--- src/main/java/com/abc/base/TestBase.java
+++ src/main/java/com/abc/base/TestBase.java
@@ -1,4 +1,67 @@
 package com.abc.base;
 
+import com.abc.util.TestUtil;
+import com.abc.util.WebEventListener;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
 public class TestBase {
+
+    protected static WebDriver driver;
+    private static Properties properties;
+
+    protected TestBase() {
+        try {
+            properties = new Properties();
+            FileInputStream fileInputStream = new FileInputStream("src/main/resources/config/config.properties");
+            properties.load(fileInputStream);
+        } catch (IOException f) {
+            f.printStackTrace();
+        }
+    }
+
+    /******************************************************************************************
+     * This method will load the browser name from properties file, opens that browser,
+     * maximizes it, deletes the cookies, sets the timeouts of the page and will redirects to
+     * desired url mentioned in the config.properties file
+     ******************************************************************************************/
+
+    protected static void initialization() {
+
+        String browserName = properties.getProperty("browserName");
+
+        if (browserName.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browserName.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else if (browserName.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        } else if (browserName.equalsIgnoreCase("ie")) {
+            WebDriverManager.iedriver().setup();
+            driver = new InternetExplorerDriver();
+        }
+
+        EventFiringWebDriver e_driver = new EventFiringWebDriver(driver);
+        WebEventListener eventListener = new WebEventListener();
+        driver = e_driver.register(eventListener);
+
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+        driver.get(properties.getProperty("url"));
+    }
 }
