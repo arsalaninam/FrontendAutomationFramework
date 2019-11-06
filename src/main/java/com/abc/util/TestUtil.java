de8a957af34876e35e3c35de67fe9ec1362b6411
Arsalan Inam <m.arsalaninam@gmail.com>
2019-11-07 01:19:11 +0500

    Add Test Util

--- src/main/java/com/abc/util/TestUtil.java
+++ src/main/java/com/abc/util/TestUtil.java
@@ -1,4 +1,38 @@
 package com.abc.util;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class TestUtil {
+
+    public static long PAGE_LOAD_TIMEOUT = 15;
+    public static long IMPLICIT_WAIT = 10;
+
+    /**********************************************
+     * Explicit Wait for visibility of an element
+     *
+     * @param driver - WebDriver instance
+     * @param element - Element to be Displayed
+     **********************************************/
+
+    public static void waitForVisibilityOfElement(WebDriver driver, WebElement element) {
+        WebDriverWait explicitWait = new WebDriverWait(driver, PAGE_LOAD_TIMEOUT);
+        explicitWait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    /*************************************************
+     * Explicit Wait for visibility of an element and
+     * enters text to the text box
+     *
+     * @param driver - WebDriver instance
+     * @param element - Element to be Displayed
+     * @param keys - Value to send in text box
+     *************************************************/
+
+    public static void sendKeysToTextBox(WebDriver driver, WebElement element, String keys) {
+        waitForVisibilityOfElement(driver, element);
+        element.sendKeys(keys);
+    }
 }
