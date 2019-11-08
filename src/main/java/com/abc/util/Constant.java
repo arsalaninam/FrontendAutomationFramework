fba93e3c99e706ba4f2ab77f22bdf885cd885789
Arsalan Inam <m.arsalaninam@gmail.com>
2019-11-08 17:38:16 +0500

    Add constants

---
+++ src/main/java/com/abc/util/Constant.java
@@ -0,0 +1,23 @@
+package com.abc.util;
+
+public interface Constant {
+
+    long IMPLICIT_WAIT = 10;
+    long PAGE_LOAD_TIMEOUT = 15;
+    long LONG_PAGE_LOAD_TIMEOUT = 50;
+    long LONGEST_PAGE_LOAD_TIMEOUT = 90;
+
+    String TEST_PREFIX = "Testing";
+
+    String PDF_FILE_PATH = "F:\\PdfSampleFile.pdf";
+    String JPG_FILE_PATH = "F:\\JpgSampleFile.jpg";
+    String PNG_FILE_PATH = "F:\\PngSampleFile.png";
+    String TIFF_FILE_PATH = "F:\\TiffSampleFile.TIFF";
+    String TXT_FILE_PATH = "F:\\PdfSampleFile.pdf";
+
+    String TEST_SUCCESSFUL_LOGIN = "--Test Login is Successful--";
+    String TEST_UNSUCCESSFUL_LOGIN = "--Test Login is Unsuccessful--";
+    String TEST_LOGIN_PAGE_TITLE = "--Test Page Title of Login Page--";
+
+    String TEST_USER_LANDS_ON_CONSOLE_PAGE = "--Test User lands on Console Page Successfully--";
+}
