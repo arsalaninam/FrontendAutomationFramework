f6eb3ae90e8e5f53e43b9eafd358f45a758ff80c
Arsalan Inam <m.arsalaninam@gmail.com>
2019-11-08 17:36:34 +0500

    Refactor login tests

--- src/test/java/com/abc/testcase/login/LoginTest.java
+++ src/test/java/com/abc/testcase/login/LoginTest.java
@@ -4,10 +4,14 @@ import com.abc.base.TestBase;
 import com.abc.data.dataprovider.LoginPageData;
 import com.abc.page.ConsolePage;
 import com.abc.page.LoginPage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import static com.abc.util.Constant.*;
 
 /**
  * Test Class to perform test cases related to Login Page
@@ -17,6 +21,7 @@ import org.testng.annotations.Test;
 
 public class LoginTest extends TestBase {
 
+    private static final Logger log = LoggerFactory.getLogger(LoginTest.class);
     private LoginPage loginPage;
 
     LoginTest() {
@@ -32,32 +37,51 @@ public class LoginTest extends TestBase {
     /*********************************************************************
      * Test to validate successful login with valid username and password
      *
-     * @param username - A valid username
-     * @param password - A valid password
+     * @param username - username test data
+     * @param password - password test data
      * @param consolePageTitle - Page Title of console page
      ********************************************************************/
-    @Test(dataProvider = "validLoginUsernamePasswordWithPageTitle", dataProviderClass = LoginPageData.class, priority = 1)
-    public void testLoginWithValidCredentials(String username, String password, String consolePageTitle) {
+
+    @Test(dataProvider = "validLoginUsernamePasswordWithPageTitle", dataProviderClass = LoginPageData.class)
+    public void testSuccessfulLogin(String username, String password, String consolePageTitle) {
+        log.info(TEST_SUCCESSFUL_LOGIN);
         loginPage.setUsername(username);
         loginPage.setPassword(password);
         ConsolePage consolePage = loginPage.clickLoginButton();
         Assert.assertEquals(consolePage.getPageTitle(), consolePageTitle);
     }
 
-    /**************************************************************************
-     * Test to validate unsuccessful login with invalid username and password
+    /*************************************************************************
+     * Test to validate unsuccessful login with
+     * - invalid username and invalid password
+     * - case sensitive username and password
      *
-     * @param username    - An invalid username
-     * @param password    - An invalid password
-     * @param failMessage - Invalid username or password fail message
+     * @param username    - username test data
+     * @param password    - password test data
+     * @param expectedFailMessage - Invalid username or password fail message
      *************************************************************************/
 
-    @Test(dataProvider = "invalidLoginUsernamePasswordWithFailMessage", dataProviderClass = LoginPageData.class, priority = 1)
-    public void testLoginWithInValidCredentials(String username, String password, String failMessage) {
+    @Test(dataProvider = "invalidLoginUsernamePasswordWithFailMessage", dataProviderClass = LoginPageData.class)
+    public void testUnsuccessfulLogin(String username, String password, String expectedFailMessage) {
+        log.info(TEST_UNSUCCESSFUL_LOGIN);
         loginPage.setUsername(username);
         loginPage.setPassword(password);
         loginPage.clickLoginButton();
-        Assert.assertEquals(loginPage.getLoginFailMessage(), failMessage);
+        String actualFailMessage = loginPage.getLoginFailMessage();
+        Assert.assertEquals(actualFailMessage, expectedFailMessage);
+    }
+
+    /*******************************************************
+     * Test to validate Title of Login Page
+     *
+     * @param expectedPageTitle - Login Page Title test data
+     *******************************************************/
+
+    @Test(dataProvider = "validLoginPageTitle", dataProviderClass = LoginPageData.class)
+    public void testLoginPageTitle(String expectedPageTitle) {
+        log.info(TEST_LOGIN_PAGE_TITLE);
+        String actualPageTitle = loginPage.getPageTitle();
+        Assert.assertEquals(actualPageTitle, expectedPageTitle);
     }
 
     @AfterMethod
