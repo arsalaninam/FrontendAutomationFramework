a139cdfa0745efdb69f46abad1bd9bce03486192
Arsalan Inam <m.arsalaninam@gmail.com>
2019-11-11 03:20:17 +0500

    Add ObjectFactory class

---
+++ src/main/java/com/abc/util/ObjectFactory.java
@@ -0,0 +1,31 @@
+package com.abc.util;
+
+import org.testng.asserts.SoftAssert;
+
+import java.util.Objects;
+
+/**
+ * Singleton object class
+ *
+ * @author Arsalan Inam
+ */
+public class ObjectFactory {
+
+    private static SoftAssert softAssert;
+
+    private ObjectFactory() {
+    }
+
+    /**
+     * If SoftAssert Class instance is not created, it creates and returns the instance.
+     * If SoftAssert CLass instance is already created, it returns the instance.
+     *
+     * @return softAssert - Singleton instance of SoftAssert Class
+     */
+    public static SoftAssert getSoftAssert() {
+        if (Objects.isNull(softAssert)) {
+            softAssert = new SoftAssert();
+        }
+        return softAssert;
+    }
+}
