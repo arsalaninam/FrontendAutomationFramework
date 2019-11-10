fabefb06a6c11e18993e79dc19b236cc3c1bc3bd
Arsalan Inam <m.arsalaninam@gmail.com>
2019-11-11 03:15:02 +0500

    Refactor TestBase and remove Property Reader

--- src/main/java/com/abc/base/TestBase.java
+++ src/main/java/com/abc/base/TestBase.java
@@ -1,5 +1,7 @@
 package com.abc.base;
 
+import com.abc.util.ObjectFactory;
+import com.abc.util.PropertyReader;
 import com.abc.util.WebEventListener;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
@@ -8,34 +10,27 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.testng.asserts.SoftAssert;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-import static com.abc.util.Constant.IMPLICIT_WAIT;
-import static com.abc.util.Constant.PAGE_LOAD_TIMEOUT;
+import static com.abc.constant.Constant.IMPLICIT_WAIT;
+import static com.abc.constant.Constant.PAGE_LOAD_TIMEOUT;
 
-public class TestBase {
+public class TestBase extends PropertyReader {
 
+    // Singleton instance of softAssert
+    protected SoftAssert softAssert = ObjectFactory.getSoftAssert();
+
+    //WebDriver instance
     protected static WebDriver driver;
-    private static Properties properties;
-
-    protected TestBase() {
-        try {
-            properties = new Properties();
-            FileInputStream fileInputStream = new FileInputStream("src/main/resources/config/config.properties");
-            properties.load(fileInputStream);
-        } catch (IOException f) {
-            f.printStackTrace();
-        }
-    }
 
     /******************************************************************************************
      * This method will load the browser name from properties file, opens that browser,
      * maximizes it, deletes the cookies, sets the timeouts of the page and will redirects to
-     * desired url mentioned in the config.properties file
+     * desired url mentioned in the config.properties file.
+     * Also, this method will register an event listener for WebDriver. It will help to log all
+     * the activities automatically that are being performed by Selenium WebDriver.
      ******************************************************************************************/
 
     protected static void initialization() {
