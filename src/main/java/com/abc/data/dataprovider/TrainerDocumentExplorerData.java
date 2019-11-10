8c9ddcf98786290a69cf418da98b86e20b69ec7f
Arsalan Inam <m.arsalaninam@gmail.com>
2019-11-11 03:16:52 +0500

    Add comments in TrainerDocumentExplorerData

--- src/main/java/com/abc/data/dataprovider/TrainerDocumentExplorerData.java
+++ src/main/java/com/abc/data/dataprovider/TrainerDocumentExplorerData.java
@@ -2,6 +2,11 @@ package com.abc.data.dataprovider;
 
 import org.testng.annotations.DataProvider;
 
+/**
+ * A data provider class to provide test data for Trainer Document Explorer related test cases
+ *
+ * @author Arsalan Inam
+ */
 public class TrainerDocumentExplorerData {
 
     @DataProvider
