00e72d3ec7a22221a31287c593e5698417570c77
Arsalan Inam <m.arsalaninam@gmail.com>
2019-11-11 03:20:55 +0500

    Add test case and refactor code in TrainerDocumentExplorerTest

--- src/test/java/com/abc/testcase/trainerDocumentExplorer/TrainerDocumentExplorerTest.java
+++ src/test/java/com/abc/testcase/trainerDocumentExplorer/TrainerDocumentExplorerTest.java
@@ -5,6 +5,9 @@ import com.abc.data.dataprovider.TrainerDocumentExplorerData;
 import com.abc.page.ConsolePage;
 import com.abc.page.LoginPage;
 import com.abc.page.TrainerDocumentExplorerPage;
+import com.abc.util.ObjectFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -12,7 +15,7 @@ import org.testng.asserts.SoftAssert;
 
 import java.util.Random;
 
-import static com.abc.util.Constant.*;
+import static com.abc.constant.Constant.*;
 
 /**
  * Test Class to perform test cases related to Trainer Document Explorer Page
@@ -22,26 +25,23 @@ import static com.abc.util.Constant.*;
 
 public class TrainerDocumentExplorerTest extends TestBase {
 
+    private static final Logger log = LoggerFactory.getLogger(TrainerDocumentExplorerTest.class);
     private LoginPage loginPage;
-    private SoftAssert softAssert = new SoftAssert();
-
-    TrainerDocumentExplorerTest() {
-        super();
-    }
+    private SoftAssert softAssert = ObjectFactory.getSoftAssert();
 
     /*************************************
      * generates 5 digit random value
      * @return - five digit integer value
      *************************************/
 
-    private int gen() {
+    private int generateNumber() {
         Random r = new Random(System.currentTimeMillis());
         return 10000 + r.nextInt(20000);
     }
 
-    /*******************
+    /********************
      * Upload file paths
-     *******************/
+     ********************/
 
     private String pdfFilePath = PDF_FILE_PATH;
     private String pngFilePath = PNG_FILE_PATH;
@@ -58,9 +58,9 @@ public class TrainerDocumentExplorerTest extends TestBase {
     /***********************************************************************************
      * Test scenario:
      * - When User enters valid credentials to Login to application successfully.
-     * - Then User is navigated to Console Page successfully.
+     * - Then validate User is navigated to Console Page successfully.
      * - When User click on Annotation icon on Console Page.
-     * - Then User is navigated to Trainer Document Explorer Page successfully.
+     * - Then validate User is navigated to Trainer Document Explorer Page successfully.
      *
      * @param username - A valid Username
      * @param password - A valid Password
@@ -90,11 +90,12 @@ public class TrainerDocumentExplorerTest extends TestBase {
     /***********************************************************************************
      * Test scenario:
      * - When User enters valid credentials to Login to application successfully.
-     * - Then User is navigated to Console Page successfully.
+     * - Then validate User is navigated to Console Page successfully.
      * - When User click on Annotation icon on Console Page.
-     * - Then User is navigated to Trainer Document Explorer Page successfully.
+     * - Then validate User is navigated to Trainer Document Explorer Page successfully.
      * - When User adds a new collection and adds a new document of pdf Type.
-     * - Then validate that new document is added successfully.
+     * - Then validate upload document type is accepted successfully
+     * - And validate that new document is added successfully.
      *
      * @param username - A valid Username
      * @param password - A valid Password
@@ -108,9 +109,8 @@ public class TrainerDocumentExplorerTest extends TestBase {
     public void testUploadPdfFileType(String username, String password,
                                       String expectedConsoleHeading,
                                       String expectedDocumentExplorerHeading) {
-        int number = gen();
-        String collectionName = TEST_PREFIX + number;
-        boolean scenarioType = true;
+        String collectionName = TEST_PREFIX + generateNumber();
+        log.info("Collection Name: " + collectionName);
 
         loginPage.setUsername(username);
         loginPage.setPassword(password);
@@ -122,9 +122,11 @@ public class TrainerDocumentExplorerTest extends TestBase {
         String actualDocumentExplorerPageHeading = trainerDocumentExplorerPage.getPageHeading();
         softAssert.assertEquals(actualDocumentExplorerPageHeading, expectedDocumentExplorerHeading);
 
-        boolean fileUploadSuccessMessage = createCollectionAndUploadDocumentS(trainerDocumentExplorerPage,
-                pdfFilePath, collectionName, scenarioType);
+        createCollectionAndUploadDocumentS(trainerDocumentExplorerPage, pdfFilePath, collectionName);
+        boolean fileTypeAcceptedSuccessfully = trainerDocumentExplorerPage.fileTypeAcceptedSuccessfullyMessageDisplayed();
+        softAssert.assertEquals(fileTypeAcceptedSuccessfully, true);
 
+        boolean fileUploadSuccessMessage = trainerDocumentExplorerPage.fileUploadSuccessfullyIsDisplayed();
         softAssert.assertEquals(fileUploadSuccessMessage, true);
         softAssert.assertAll();
     }
@@ -132,11 +134,12 @@ public class TrainerDocumentExplorerTest extends TestBase {
     /***********************************************************************************
      * Test scenario:
      * - When User enters valid credentials to Login to application successfully.
-     * - Then User is navigated to Console Page successfully.
+     * - Then validate User is navigated to Console Page successfully.
      * - When User click on Annotation icon on Console Page.
-     * - Then User is navigated to Trainer Document Explorer Page successfully.
+     * - Then validate User is navigated to Trainer Document Explorer Page successfully.
      * - When User adds a new collection and adds a new document of jpg Type.
-     * - Then validate that new document is not added successfully.
+     * - Then validate upload document type is accepted successfully
+     * - And validate that new document is not added successfully.
      *
      * @param username - A valid Username
      * @param password - A valid Password
@@ -150,9 +153,8 @@ public class TrainerDocumentExplorerTest extends TestBase {
     public void testUploadJpgFileType(String username, String password,
                                       String expectedConsoleHeading,
                                       String expectedDocumentExplorerHeading) {
-        int number = gen();
-        String collectionName = TEST_PREFIX + number;
-        boolean scenarioType = false;
+        String collectionName = TEST_PREFIX + generateNumber();
+        log.info("Collection Name: " + collectionName);
 
         loginPage.setUsername(username);
         loginPage.setPassword(password);
@@ -164,9 +166,11 @@ public class TrainerDocumentExplorerTest extends TestBase {
         String actualDocumentExplorerPageHeading = trainerDocumentExplorerPage.getPageHeading();
         softAssert.assertEquals(actualDocumentExplorerPageHeading, expectedDocumentExplorerHeading);
 
-        boolean fileUploadFailedMessage = createCollectionAndUploadDocumentS(trainerDocumentExplorerPage,
-                jpgFilePath, collectionName, scenarioType);
+        createCollectionAndUploadDocumentS(trainerDocumentExplorerPage, jpgFilePath, collectionName);
+        boolean fileTypeAcceptedSuccessfully = trainerDocumentExplorerPage.fileTypeAcceptedSuccessfullyMessageDisplayed();
+        softAssert.assertEquals(fileTypeAcceptedSuccessfully, true);
 
+        boolean fileUploadFailedMessage = trainerDocumentExplorerPage.fileUploadFailedIsDisplayed();
         softAssert.assertEquals(fileUploadFailedMessage, true);
         softAssert.assertAll();
     }
@@ -174,11 +178,12 @@ public class TrainerDocumentExplorerTest extends TestBase {
     /***********************************************************************************
      * Test scenario:
      * - When User enters valid credentials to Login to application successfully.
-     * - Then User is navigated to Console Page successfully.
+     * - Then validate User is navigated to Console Page successfully.
      * - When User click on Annotation icon on Console Page.
-     * - Then User is navigated to Trainer Document Explorer Page successfully.
+     * - Then validate User is navigated to Trainer Document Explorer Page successfully.
      * - When User adds a new collection and adds a new document of png Type.
-     * - Then validate that new document is added successfully.
+     * - Then validate upload document type is accepted successfully
+     * - And validate that new document is not added successfully.
      *
      * @param username - A valid Username
      * @param password - A valid Password
@@ -192,9 +197,8 @@ public class TrainerDocumentExplorerTest extends TestBase {
     public void testUploadPngFileType(String username, String password,
                                       String expectedConsoleHeading,
                                       String expectedDocumentExplorerHeading) {
-        int number = gen();
-        String collectionName = TEST_PREFIX + number;
-        boolean scenarioType = false;
+        String collectionName = TEST_PREFIX + generateNumber();
+        log.info("Collection Name: " + collectionName);
 
         loginPage.setUsername(username);
         loginPage.setPassword(password);
@@ -206,21 +210,25 @@ public class TrainerDocumentExplorerTest extends TestBase {
         String actualDocumentExplorerPageHeading = trainerDocumentExplorerPage.getPageHeading();
         softAssert.assertEquals(actualDocumentExplorerPageHeading, expectedDocumentExplorerHeading);
 
-        boolean fileUploadFailedMessage = createCollectionAndUploadDocumentS(trainerDocumentExplorerPage,
-                pngFilePath, collectionName, scenarioType);
+        createCollectionAndUploadDocumentS(trainerDocumentExplorerPage, pngFilePath, collectionName);
+        boolean fileTypeAcceptedSuccessfully = trainerDocumentExplorerPage.fileTypeAcceptedSuccessfullyMessageDisplayed();
+        softAssert.assertEquals(fileTypeAcceptedSuccessfully, true);
 
+        boolean fileUploadFailedMessage = trainerDocumentExplorerPage.fileUploadFailedIsDisplayed();
         softAssert.assertEquals(fileUploadFailedMessage, true);
+        softAssert.assertAll();
         softAssert.assertAll();
     }
 
     /***********************************************************************************
      * Test scenario:
      * - When User enters valid credentials to Login to application successfully.
-     * - Then User is navigated to Console Page successfully.
+     * - Then validate User is navigated to Console Page successfully.
      * - When User click on Annotation icon on Console Page.
-     * - Then User is navigated to Trainer Document Explorer Page successfully.
+     * - Then validate User is navigated to Trainer Document Explorer Page successfully.
      * - When User adds a new collection and adds a new document of tiff Type.
-     * - Then validate that new document is added successfully.
+     * - Then validate upload document type is accepted successfully
+     * - And validate that new document is not added successfully.
      *
      * @param username - A valid Username
      * @param password - A valid Password
@@ -234,9 +242,8 @@ public class TrainerDocumentExplorerTest extends TestBase {
     public void testUploadTiffFileType(String username, String password,
                                        String expectedConsoleHeading,
                                        String expectedDocumentExplorerHeading) {
-        int number = gen();
-        String collectionName = TEST_PREFIX + number;
-        boolean scenarioType = false;
+        String collectionName = TEST_PREFIX + generateNumber();
+        log.info("Collection Name: " + collectionName);
 
         loginPage.setUsername(username);
         loginPage.setPassword(password);
@@ -248,28 +255,67 @@ public class TrainerDocumentExplorerTest extends TestBase {
         String actualDocumentExplorerPageHeading = trainerDocumentExplorerPage.getPageHeading();
         softAssert.assertEquals(actualDocumentExplorerPageHeading, expectedDocumentExplorerHeading);
 
-        boolean fileUploadFailedMessage = createCollectionAndUploadDocumentS(trainerDocumentExplorerPage,
-                tiffFilePath, collectionName, scenarioType);
+        createCollectionAndUploadDocumentS(trainerDocumentExplorerPage, tiffFilePath, collectionName);
+        boolean fileTypeAcceptedSuccessfully = trainerDocumentExplorerPage.fileTypeAcceptedSuccessfullyMessageDisplayed();
+        softAssert.assertEquals(fileTypeAcceptedSuccessfully, true);
 
+        boolean fileUploadFailedMessage = trainerDocumentExplorerPage.fileUploadFailedIsDisplayed();
         softAssert.assertEquals(fileUploadFailedMessage, true);
         softAssert.assertAll();
     }
 
-    /***
-     * This method with create a new collection and Upload a new document in the collection.
+    /***********************************************************************************
+     * Test scenario:
+     * - When User enters valid credentials to Login to application successfully.
+     * - Then validate User is navigated to Console Page successfully.
+     * - When User click on Annotation icon on Console Page.
+     * - Then validate User is navigated to Trainer Document Explorer Page successfully.
+     * - When User adds a new collection and adds a new document of txt Type.
+     * - Then validate message "Rejected (because of type)" is displayed.
+     *
+     * @param username - A valid Username
+     * @param password - A valid Password
+     * @param expectedConsoleHeading - Console Page Heading
+     * @param expectedDocumentExplorerHeading - Trainer Document Explorer Page Heading
+     ***********************************************************************************/
+
+    @Test(dataProvider = "validUsernamePasswordAndPageHeadings",
+            dataProviderClass = TrainerDocumentExplorerData.class,
+            dependsOnMethods = "testUserNavigatesToTrainerDocumentExplorerPage")
+    public void testUploadTxtFileType(String username, String password,
+                                      String expectedConsoleHeading,
+                                      String expectedDocumentExplorerHeading) {
+        String collectionName = TEST_PREFIX + generateNumber();
+        log.info("Collection Name: " + collectionName);
+
+        loginPage.setUsername(username);
+        loginPage.setPassword(password);
+        ConsolePage consolePage = loginPage.clickLoginButton();
+        String actualConsolePageHeading = consolePage.getPageHeading();
+        softAssert.assertEquals(actualConsolePageHeading, expectedConsoleHeading);
+
+        TrainerDocumentExplorerPage trainerDocumentExplorerPage = consolePage.clickOnAnnotateIcon();
+        String actualDocumentExplorerPageHeading = trainerDocumentExplorerPage.getPageHeading();
+        softAssert.assertEquals(actualDocumentExplorerPageHeading, expectedDocumentExplorerHeading);
+
+        createCollectionAndUploadDocumentS(trainerDocumentExplorerPage, txtFilePath, collectionName);
+        boolean fileTypeRejectedMessageDisplayed = trainerDocumentExplorerPage.fileTypeRejectedMessageDisplayed();
+        softAssert.assertEquals(fileTypeRejectedMessageDisplayed, true);
+        softAssert.assertAll();
+    }
+
+    /**********************************************************************
+     * This method creates a new collection and
+     * Uploads a valid new document in the collection.
      *
      * @param trainerDocumentExplorerPage - Trainer Document Page instance
      * @param filePath - Absolute file path to upload file
      * @param collectionName - Name of new collection
-     * @param scenarioType - Type of Scenario to return successful or failed image check
-     * @return - Returns success image check if scenario Type is true
-     * @return - Or Returns faik image check if scenario Type is false
-     */
+     **********************************************************************/
 
-    private boolean createCollectionAndUploadDocumentS(TrainerDocumentExplorerPage trainerDocumentExplorerPage,
-                                                       String filePath,
-                                                       String collectionName,
-                                                       boolean scenarioType) {
+    private void createCollectionAndUploadDocumentS(TrainerDocumentExplorerPage trainerDocumentExplorerPage,
+                                                    String filePath,
+                                                    String collectionName) {
         trainerDocumentExplorerPage.clickOnCollectionsBtn();
         trainerDocumentExplorerPage.createNewCollection(collectionName);
         trainerDocumentExplorerPage.clickCreateNewCollectionBtn();
@@ -277,10 +323,6 @@ public class TrainerDocumentExplorerTest extends TestBase {
         trainerDocumentExplorerPage.clickOnSearchedDocumentFromCollectionsTable();
         trainerDocumentExplorerPage.checkVisibilityOfUploadButton();
         trainerDocumentExplorerPage.uploadFile(filePath);
-        if (scenarioType) {
-            return trainerDocumentExplorerPage.checkFileUploadSuccessfullyIsDisplayed();
-        }
-        return trainerDocumentExplorerPage.checkFileUploadFailedIsDisplayed();
     }
 
     @AfterMethod
